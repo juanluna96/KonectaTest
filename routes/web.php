c285baa5cbabf1f227d9bc311826a275528e9fd6
juanluna96 <juandluna17@gmail.com>
2021-01-05 08:21:52 -0500

    Modificadion sidebar admin y administrador de usuarios

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,14 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('users/admins', 'UserController@admins')->name('users.admins');
+Route::get('users/sellers', 'UserController@sellers')->name('users.sellers');
+Route::get('users/clients', 'UserController@clients')->name('users.clients');
+Route::get('/users/create', 'UserController@create')->name('recetas.create');
+Route::post('/recetas', 'UserController@store')->name('recetas.store');
+Route::get('/recetas/{receta}', 'UserController@show')->name('recetas.show');
+Route::get('/recetas/{receta}/edit', 'UserController@edit')->name('recetas.edit');
+Route::put('/recetas/{receta}', 'UserController@update')->name('recetas.update');
+Route::delete('/recetas/{receta}', 'UserController@destroy')->name('recetas.destroy');
+
 Route::get('/home', 'HomeController@index')->name('home');
