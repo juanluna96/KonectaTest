b00a0c84f68a19b4866d74945fac126a63e9f9d0
juanluna96 <juandluna17@gmail.com>
2021-01-04 23:29:04 -0500

    Creacion de roles

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'rol_id'
     ];
 
     /**
@@ -36,6 +36,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /* ------------------------------ Relationships ----------------------------- */
+
+    public function rol()
+    {
+        return $this->hasOne(Rol::class);
+    }
 
     /* ---------------------------- AdminLTE Methods ---------------------------- */
 
